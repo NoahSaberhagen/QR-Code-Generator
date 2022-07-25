0104d8532627d6b05a772ac5e652f71afac19e2b
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-07-25 02:30:17 +0000

    begin setup of cors proxy server for development purposes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  
+export default function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchAPI = async () => {
+    await fetch(`https://www.qrcode-monkey.com/qr/custom`, {
+      method: "POST",
+      config:{
+        body: "circle",
+        logo: "#facebook"
+        },
+        size: 300,
+        download: false,
+        file: "svg"
+    })
+      .then((response) => response.json())
+      .then((result) => console.log(result));
+  }
+
+  useEffect(fetchAPI, []);
 }
 
-export default App
